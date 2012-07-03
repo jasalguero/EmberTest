3efb6d8d3ff9df52810e80eb47e75fd297f2b9b3
Jose Salguero <jasalguero@gmail.com>
2012-07-04 01:03:51 +0200

    Added expand/collapse functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 var App = Em.Application.create();
 
 App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  }
+});
+
+
+/******************************************************/
+/*				BASIC FUNCTIONALITY					  */
+/******************************************************/
+$(document).ready(function() {
+    
+	//Add event for expand and collapse post content
+	$(".postMore .toggleButton").on("click", function(){
+		$(this).closest(".post").find(".fullText").fadeToggle("400","linear");
+		$(this).parent().children(".toggleButton").toggle();
+	});
+
 });
