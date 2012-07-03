f5c763e68ae3182c6ab6bc0f27bd961bdf6ec184
Jose Salguero <jasalguero@gmail.com>
2012-07-04 01:51:46 +0200

    Moved "more" button

--- js/app.js
+++ js/app.js
@@ -3,16 +3,17 @@ var App = Em.Application.create();
 App.MyView = Em.View.extend({
 });
 
-
 /******************************************************/
 /*				BASIC FUNCTIONALITY					  */
 /******************************************************/
 $(document).ready(function() {
-    
+
 	//Add event for expand and collapse post content
-	$(".postMore .toggleButton").on("click", function(){
-		$(this).closest(".post").find(".fullText").fadeToggle("400","linear");
-		$(this).parent().children(".toggleButton").toggle();
+	$(".postMore .toggleButton").on("click", function() {
+		$(this).closest(".post").find(".fullText").fadeToggle("400", "linear", function() {		
+			$(this).closest(".post").find(".toggleButton").toggle();
+		});
+
 	});
 
-});
+}); 
