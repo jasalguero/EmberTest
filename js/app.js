1e4a69ceb038cbe5f03d06f2c2f56330a8cc3479
Jose Salguero <jasalguero@gmail.com>
2012-07-27 09:04:08 +0200

    Started ember model

--- js/app.js
+++ js/app.js
@@ -1,19 +1,72 @@
-var App = Em.Application.create();
+/******************************************************/
+/*				INITS					  */
+/******************************************************/
+var HaBlog = Em.Application.create({
 
-App.MyView = Em.View.extend({
 });
 
 /******************************************************/
-/*				BASIC FUNCTIONALITY					  */
+/*				MODEL								  */
 /******************************************************/
-$(document).ready(function() {
 
-	//Add event for expand and collapse post content
-	$(".postMore .toggleButton").on("click", function() {
-		$(this).closest(".post").find(".fullText").fadeToggle("400", "linear", function() {		
-			$(this).closest(".post").find(".toggleButton").toggle();
-		});
+// POST ITEM
+HaBlog.Post = Em.Object.extend({
+	uid : null,
+	title : null,
+	state : null,
+	created : null,
+	title : null,
+	headline : null,
+	content : null,
+	tags : null
+});
 
-	});
+/******************************************************/
+/*				CONTROLLERS							  */
+/******************************************************/
 
-}); 
+// DATA CONTROLLER
+HaBlog.dataController = Em.ArrayController.create({
+	//container with the list of posts
+	posts : [],
+
+	//add a new post to the list checking that it was not previously there and ordered by creationg date
+	addPost : function(post) {
+		// Check to see if there are any post in the controller with the same uid already
+		var exists = this.filterProperty('uid', post.uid).length;
+		if (exists === 0) {
+			// If no results are returned, we insert the new item into the data controller in order of publication date
+			var length = this.get('length'), idx;
+			idx = this.binarySearch(Date.parse(post.get('created').get('time')), 0, length);
+			this.insertAt(idx, post);
+			return true;
+		}
+	},
+	// Binary search implementation that finds the index where a entry
+	// should be inserted when sorting by date.
+	binarySearch : function(value, low, high) {
+		var mid, midValue;
+		if (low === high) {
+			return low;
+		}
+		mid = low + Math.floor((high - low) / 2);
+		midValue = Date.parse(this.objectAt(mid).get('created').get('time'));
+
+		if (value < midValue) {
+			return this.binarySearch(value, mid + 1, high);
+		} else if (value > midValue) {
+			return this.binarySearch(value, low, mid);
+		}
+		return mid;
+	},
+});
+
+/******************************************************/
+/*				VIEWS								  */
+/******************************************************/
+// View for the Post list
+HaBlog.PostSummaryListView = Em.View.extend({
+	tagName: 'article',
+	classNames: ['well', 'summary']
+});
+
---
+++ js/hablog.js
@@ -0,0 +1,12 @@
+/******************************************************/
+/*				BASIC FUNCTIONALITY					  */
+/******************************************************/
+$(document).ready(function() {
+
+	//Add event for expand and collapse post content
+	$(".postMore .toggleButton").on("click", function() {
+		$(this).closest(".post").find(".fullText").fadeToggle("400", "linear", function() {		
+			$(this).closest(".post").find(".toggleButton").toggle();
+		});
+	});
+}); 
