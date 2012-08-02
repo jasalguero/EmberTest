f22ff5cba94499c3a73fb6487695a2889beeacb6
Jose Salguero <jasalguero@gmail.com>
2012-08-03 00:28:32 +0200

    Added date library Linking ember model with html view

--- js/app.js
+++ js/app.js
@@ -2,7 +2,30 @@
 /*				INITS					  */
 /******************************************************/
 var HaBlog = Em.Application.create({
+	ready : function() {
+		testPost = HaBlog.Post.create({
+			uid : "1",
+			title : "Test Title",
+			headline : "headline",
+			tags : [{
+				tag : 'blog'
+			}, {
+				tag : 'write'
+			}],
+			author : 'pprang',
+			sections : [{
+				text : 'It would have been so easy -- just pick a blogging platform and\n voila one has a blog with a lot of functionality. In case you really want to own it just roll out one of the many blogging suites on your server. But **none** can boast of the pain and joy creating your own software\n        brings with it. The creativity, the decision making, the struggles to implement the vision at the micro and macro level,\n        and other nitty gritties spice up the adventure.'
+			}, {
+				headline : 'The need',
+				text : "<p class =\"bla\">[Twitter](http://twitter.com) is too terse;\n most journals have more ads then real content;\n a private diary isn't social enough.</p><p>A good blog is about content, content and content.\n A great or unique writing style makes the difference when content is not a problem.\n        All other things equal a great design and technological schnickschnack give the blog that aura of invincibility.</p>"
+			}],
+			//created : moment.unix(1318781876)
+			created : moment()
+		});
 
+		HaBlog.postsController.addPost(testPost);
+
+	}
 });
 
 /******************************************************/
@@ -14,11 +37,12 @@ HaBlog.Post = Em.Object.extend({
 	uid : null,
 	title : null,
 	state : null,
-	created : null,
+	author : null,
+	created : new Date(0),
 	title : null,
 	headline : null,
-	content : null,
-	tags : null
+	sections : null,
+	tags : null,
 });
 
 /******************************************************/
@@ -26,9 +50,9 @@ HaBlog.Post = Em.Object.extend({
 /******************************************************/
 
 // DATA CONTROLLER
-HaBlog.dataController = Em.ArrayController.create({
+HaBlog.postsController = Em.ArrayController.create({
 	//container with the list of posts
-	posts : [],
+	content : [],
 
 	//add a new post to the list checking that it was not previously there and ordered by creationg date
 	addPost : function(post) {
@@ -37,7 +61,7 @@ HaBlog.dataController = Em.ArrayController.create({
 		if (exists === 0) {
 			// If no results are returned, we insert the new item into the data controller in order of publication date
 			var length = this.get('length'), idx;
-			idx = this.binarySearch(Date.parse(post.get('created').get('time')), 0, length);
+			idx = this.binarySearch(Date.parse(post.get('created')), 0, length);
 			this.insertAt(idx, post);
 			return true;
 		}
@@ -66,7 +90,26 @@ HaBlog.dataController = Em.ArrayController.create({
 /******************************************************/
 // View for the Post list
 HaBlog.PostSummaryListView = Em.View.extend({
-	tagName: 'article',
-	classNames: ['well', 'summary']
+	tagName : 'article',
+	template : 'post-list-view',
+	// Returns the creation date
+	creationDay : function() {
+		var d = this.get('content').get('created');
+		return moment(d).format('DD');
+	}.property('HaBlog.postsController.@each.created'),
+	creationMonth : function() {
+		var d = this.get('content').get('created');
+		return moment(d).format('MMM');
+	}.property('HaBlog.postsController.@each.created'),
+	creationYear : function() {
+		var d = this.get('content').get('created');
+		return moment(d).format('YYYY');
+	}.property('HaBlog.postsController.@each.created'),
+	toggleButtons : function() {
+		console.log($(this))
+		$(this).closest(".post").find(".fullText").fadeToggle("400", "linear", function() {
+			$(this).closest(".post").find(".toggleButton").toggle();
+		});
+	}
 });
 
